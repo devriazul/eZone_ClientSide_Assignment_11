9083af4184cda7b25b0e55c06f585e63e3957a3f
devriazul <engr.riazul@gmail.com>
2022-05-21 21:10:29 +0600

    Component imported

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import AddItem from './components/AddItem/AddItem';
+import MyItems from './components/MyItems/MyItems';
+import Login from './Pages/Login/Login';
+import ManageItems from './components/ManageItems/ManageItems';
+import About from './components/About/About';
+import Blogs from './components/Blogs/Blogs';
+import Footer from './components/Footer/Footer';
+import Update from './components/Update/Update';
+import Signup from './Pages/Signup/Signup';
+import NotFound from './components/NotFound/NotFound';
+import RequireAuth from './Pages/RequireAuth/RequireAuth';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
